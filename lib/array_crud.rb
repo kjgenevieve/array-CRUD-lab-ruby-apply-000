5d238dd8940c10b95b4f6cd0d509fedb20ae42c1
Genevieve <kj.genevieve@gmail.com>
2019-07-02 15:35:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  please_add_to_this_array = []
+  please_add_to_this_array.push("q")
+  puts please_add_to_this_array
 end
 
 def add_element_to_start_of_array(array, element)
